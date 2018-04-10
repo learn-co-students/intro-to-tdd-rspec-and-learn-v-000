39ff52d49d86add7b7a174f6bbb3f004de703234
Julian Johannesen <julian.johannesen@gmail.com>
2018-04-10 20:17:20 +0000

    Recommended additional changes

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,10 @@
-#puts "What year were you born?"
-#birth_year = gets.chomp.to_i
 def current_age_for_birth_year(birth_year)
-  return 2003 - birth_year
+  age = Time.now.year - birth_year
+  puts "You are #{age} years old."
+end
+
+puts "What year were you born?"
+birth_year = gets.to_i
+if birth_year
+  current_age_for_birth_year(birth_year)
 end
