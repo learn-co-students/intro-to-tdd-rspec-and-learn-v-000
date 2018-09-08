acd52cdbdd5e204afd9feba8246be3d4ec365798
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-08 14:44:19 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -7,3 +7,14 @@ describe "current_age_for_birth_year method" do
     expect(age_of_person).to eq(19)
   end
 end
+
+
+#describe "current_age_for_birth_year method" do
+  #it "returns the age of a person based on the year of birth" do
+    #now = Time.new("2018-01-01 13:00:00")
+    #allow(Time).to receive(:now) { now }
+    #age_of_person = current_age_for_birth_year(1998)
+
+    #expect(age_of_person).to eq(20)
+  #end
+#end
